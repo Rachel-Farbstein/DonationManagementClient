f32f5012c1402071d57d170b2130918fad3422f4
Rachel Farbstein <rachely7331@gmail.com>
2024-12-08 15:24:02 +0200

    Merge from master

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 
 @Component({
@@ -8,26 +9,45 @@ import { MenuItem } from 'primeng/api';
 })
 export class SideMenuComponent implements OnInit {
 
-  items: MenuItem[] | undefined;
+  constructor(private router: Router) {
+
+  }
+
+  isCollapsed: boolean = false;
+  @Output() toggleMenu = new EventEmitter<boolean>();
+  menuItems: any[] | undefined;
 
   ngOnInit() {
-    this.items = [
+    this.menuItems = [
       {
         label: 'בית',
-        route: '/dashboard',
+        route: ['/dashboard'],
         icon: 'pi pi-home'
       },
       {
         label: 'תורמים',
-        route: ['donors'],
+        route: ['/dashboard', 'donors'],
         icon: 'pi pi-address-book'
       },
       {
         label: 'תרומות',
-        route: '/donates',
+        route: ['dashboard', 'donates'],
         icon: 'pi pi-dollar'
       }
     ];
   }
 
+  toggleMenuState() {
+    this.isCollapsed = !this.isCollapsed;
+    this.toggleMenu.emit(this.isCollapsed);
+  }
+
+  // navigateTo(route: string): void {
+  //   this.router.navigate([route]); // Navigate using Angular Router
+  //   // this.router.navigate(['dashboard', route]);
+  // }
+
+  // isActive(route: string): boolean {
+  //   return this.router.url === route;
+  // }
 }
